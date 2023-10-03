4c7419e4579810fa581b1c0c056e81da7eaa1f15
Arjan Frans <arjan.frans@fusonic.net>
2023-10-03 20:10:49 +0200

    add leagues

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,23 @@
-export const CONFIG = {
+export interface Config {
+    output: string;
+    defaultGameDuration: number;
+    leagues: Array<{
+        year: number;
+        name: string;
+        games: string[];
+        shortName: string;
+        standings: string;
+        slug: string
+    }>,
+    timezone: string;
+    crawlYears: number[]
+}
+
+export const CONFIG: Config = {
     output: '../public/api/',
+    crawlYears: [2023],
+    timezone: 'Europe/Vienna',
+    defaultGameDuration: 120,
     leagues: [
         {
             year: 2023,
@@ -7,7 +25,7 @@ export const CONFIG = {
             shortName: 'NL/BBL Playoffs',
             slug: 'nl-bbl-playoffs',
             standings: 'https://www.baseballsoftball.at/de/events/baseball-nationalliga-baseball-bundesliga-playoffs-2023/standings',
-            games: 'https://www.baseballsoftball.at/de/events/baseball-nationalliga-baseball-bundesliga-playoffs-2023/calendars?round=&team=27722&date='
+            games: ['https://www.baseballsoftball.at/de/events/baseball-nationalliga-baseball-bundesliga-playoffs-2023/calendars?round=&team=27722&date=']
         },
         {
             year: 2023,
@@ -15,7 +33,7 @@ export const CONFIG = {
             shortName: 'BBL',
             slug: 'bbl',
             standings: 'https://www.baseballsoftball.at/de/events/baseball-bundesliga-2023/standings',
-            games: 'https://www.baseballsoftball.at/de/events/baseball-bundesliga-2023/calendars?round=&team=24492&date='
+            games: ['https://www.baseballsoftball.at/de/events/baseball-bundesliga-2023/calendars?round=&team=24492&date=']
         },
         {
             year: 2023,
@@ -23,7 +41,62 @@ export const CONFIG = {
             shortName: '2. BLW',
             slug: '2-blw',
             standings: 'https://www.baseballsoftball.at/de/events/baseball-2-bundesliga-west-2023/standings',
-            games: 'https://www.baseballsoftball.at/de/events/baseball-2-bundesliga-west-2023/calendars?round=&team=24618&date='
+            games: ['https://www.baseballsoftball.at/de/events/baseball-2-bundesliga-west-2023/calendars?round=&team=24618&date=']
+        },
+        {
+            year: 2023,
+            name: 'Landesliga Vorarlberg',
+            shortName: 'LLV',
+            slug: 'llv',
+            standings: 'https://www.baseballsoftball.at/de/events/baseball-landesliga-vorarlberg-2023/standings',
+            games: ['https://www.baseballsoftball.at/de/events/baseball-landesliga-vorarlberg-2023/calendars?round=&team=24636&date=']
+        },
+        {
+            year: 2023,
+            name: 'Baseball Jugendliga Vorarlberg U16',
+            shortName: 'U16',
+            slug: 'u16',
+            standings: 'https://www.baseballsoftball.at/de/events/baseball-jugendliga-vorarlberg-u16-2023/standings',
+            games: ['https://www.baseballsoftball.at/de/events/baseball-jugendliga-vorarlberg-u16-2023/calendars?round=&team=24820']
+        },
+        {
+            year: 2023,
+            name: 'Baseball Ponyliga Vorarlberg U14',
+            shortName: 'U14',
+            slug: 'u14',
+            standings: 'https://www.baseballsoftball.at/de/events/baseball-ponyliga-vorarlberg-u14-2023/standings',
+            games: ['https://www.baseballsoftball.at/de/events/baseball-ponyliga-vorarlberg-u14-2023/calendars?round=&team=24821']
+        },
+        {
+            year: 2023,
+            name: 'Baseball Schülerliga Vorarlberg U13',
+            shortName: 'U13',
+            slug: 'u13',
+            standings: 'https://www.baseballsoftball.at/de/events/baseball-schuelerliga-vorarlberg-u13-2023/standings',
+            games: [
+                'https://www.baseballsoftball.at/de/events/baseball-schuelerliga-vorarlberg-u13-2023/calendars?round=&team=24823&date=',
+                'https://www.baseballsoftball.at/de/events/baseball-schuelerliga-vorarlberg-u13-2023/calendars?round=&team=24822&date='
+            ]
+        },
+        {
+            year: 2023,
+            name: 'Baseball Schülerliga Vorarlberg U10',
+            shortName: 'U10',
+            slug: 'u10',
+            standings: 'https://www.baseballsoftball.at/de/events/baseball-schuelerliga-vorarlberg-u10-2023/standings',
+            games: [
+                'https://www.baseballsoftball.at/de/events/baseball-schuelerliga-vorarlberg-u10-2023/calendars?round=&team=24824',
+            ]
+        },
+        {
+            year: 2023,
+            name: 'Beeball Vorarlberg U8',
+            shortName: 'U8',
+            slug: 'u8',
+            standings: 'https://www.baseballsoftball.at/de/events/beeball-vorarlberg-u8-2023/standings',
+            games: [
+                'https://www.baseballsoftball.at/de/events/beeball-vorarlberg-u8-2023/calendars?round=&team=26067&date=',
+            ]
         }
     ]
 }
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,43 @@
-import {GameCrawler} from "@hardbulls/wbsc-crawler";
+import {Game, GameCrawler, GameStatus, StandingsCrawler} from "@hardbulls/wbsc-crawler";
 import {CONFIG} from "./config";
 import * as fs from 'fs/promises'
 import * as path from "path";
+import {IcalGenerator} from "./Calendar/IcalGenerator";
 
-
-const fetchGames = async () => {
-    for (const league of CONFIG.leagues) {
-        const games = await GameCrawler.crawl(league.games)
-
-        const outputDirectory =   path.resolve(path.join(__dirname, CONFIG.output, 'games', league.year.toString()));
-        const outputFile = path.resolve(outputDirectory, `${league.slug}.json`,);
-
-        await fs.mkdir(outputDirectory, { recursive: true })
-        await fs.writeFile(outputFile, JSON.stringify(games));
-    }
-}
+const now = new Date();
+const today = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1, 0, 0, 0);
 
 (async () => {
-    await fetchGames()
+    const leagues = CONFIG.leagues.filter(v => CONFIG.crawlYears.includes(v.year))
+    const nextWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 7, 0, 0, 0);
+    const upcomingWeekGames: Game[] = [];
+    const baseOutputDir = path.join(__dirname, CONFIG.output)
+
+    for (const league of leagues) {
+        const leagueName = `${league.name} ${league.year}`
+        const games = (await Promise.all(league.games.map(async gameUrl => await GameCrawler.crawl(gameUrl)))).flat()
+        const standings = await StandingsCrawler.crawl(league.standings)
+
+        const leagueDirectory = path.resolve(path.join(baseOutputDir, 'seasons', league.year.toString(), league.slug));
+        const gamesFile = path.resolve(leagueDirectory, `games.json`,);
+        const standingsFile = path.resolve(leagueDirectory, `standings.json`,);
+
+        await fs.mkdir(leagueDirectory, {recursive: true})
+        await fs.writeFile(gamesFile, JSON.stringify(games, null, 2));
+        await fs.writeFile(standingsFile, JSON.stringify(standings, null, 2));
+
+        for (const game of games) {
+            if (game.date > today && game.date < nextWeek && game.status === GameStatus.SCHEDULED) {
+                upcomingWeekGames.push(game);
+            }
+        }
+
+        const leagueCalendar = IcalGenerator.games(leagueName, games.filter(v => v.status === GameStatus.SCHEDULED || v.status === GameStatus.FINISHED), CONFIG.timezone, CONFIG.defaultGameDuration)
+
+        await fs.writeFile(path.resolve(leagueDirectory, 'games.ics'), leagueCalendar);
+    }
+
+    await fs.writeFile(path.resolve(baseOutputDir, 'weekly-games.json'), JSON.stringify(upcomingWeekGames, null, 2));
 })()
 
 

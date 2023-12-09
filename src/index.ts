d8d3df6af35e5b1d98eb695b5e3022843cf03fd4
Arjan Frans <arjan.frans@fusonic.net>
2023-12-09 13:19:30 +0100

    add past statistics

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {Game, GameCrawler, GameStatus, StandingsCrawler} from "@hardbulls/wbsc-crawler";
+import {Game, GameCrawler, GameStatus, StandingsCrawler, StatisticsCrawler} from "@hardbulls/wbsc-crawler";
 import {CONFIG} from "./config";
 import * as fs from 'fs/promises'
 import * as path from "path";
@@ -7,52 +7,112 @@ import {IcalGenerator} from "./Calendar/IcalGenerator";
 const now = new Date();
 const today = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1, 0, 0, 0);
 
-interface ApiGame extends Game{
+interface ApiGame extends Game {
     league: string,
     season: number,
 }
 
 (async () => {
-    const leagues = CONFIG.leagues.filter(v => CONFIG.crawlYears.includes(v.year))
     const nextWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 7, 0, 0, 0);
     const upcomingWeekGames: ApiGame[] = [];
     const baseOutputDir = path.join(__dirname, CONFIG.output)
 
-    for (const league of leagues) {
+    for (const league of CONFIG.leagues) {
         const leagueName = `${league.name} ${league.year}`
-        const games: ApiGame[] = (await Promise.all(league.games.map(async gameUrl => await GameCrawler.crawl(gameUrl, CONFIG.timezone)))).flat().map(game => {
-            return {
-                ...game,
-                league: league.slug,
-                season: league.year
-            }
-        })
-        const standings = await StandingsCrawler.crawl(league.standings)
 
-        const leagueDirectory = path.resolve(path.join(baseOutputDir, 'seasons', league.year.toString(), league.slug));
-        const gamesFile = path.resolve(leagueDirectory, `games.json`,);
-        const standingsFile = path.resolve(leagueDirectory, `standings.json`,);
+        if (CONFIG.crawlYears.includes(league.year)) {
 
-        await fs.mkdir(leagueDirectory, {recursive: true})
-        await fs.writeFile(gamesFile, JSON.stringify(games, null, 2));
-        await fs.writeFile(standingsFile, JSON.stringify(standings, null, 2));
-
-        for (const game of games) {
-            if (game.date > today && game.date < nextWeek && game.status === GameStatus.SCHEDULED) {
-                upcomingWeekGames.push({
+            const games: ApiGame[] = (await Promise.all(league.games.map(async gameUrl => await GameCrawler.crawl(gameUrl, CONFIG.timezone)))).flat().map(game => {
+                return {
                     ...game,
                     league: league.slug,
                     season: league.year
-                });
+                }
+            })
+            const standings = await StandingsCrawler.crawl(league.standings)
+
+            const leagueDirectory = path.resolve(path.join(baseOutputDir, 'seasons', league.year.toString(), league.slug));
+            const gamesFile = path.resolve(leagueDirectory, `games.json`,);
+            const standingsFile = path.resolve(leagueDirectory, `standings.json`,);
+            const statisticsFile = path.resolve(leagueDirectory, `statistics.json`,);
+
+            await fs.mkdir(leagueDirectory, {recursive: true})
+            await fs.writeFile(gamesFile, JSON.stringify(games, null, 2));
+            await fs.writeFile(standingsFile, JSON.stringify(standings, null, 2));
+
+            for (const game of games) {
+                if (game.date > today && game.date < nextWeek && game.status === GameStatus.SCHEDULED) {
+                    upcomingWeekGames.push({
+                        ...game,
+                        league: league.slug,
+                        season: league.year
+                    });
+                }
             }
+
+            if (league.statistics) {
+                const statistics = await StatisticsCrawler.crawl(league.statistics)
+
+                await fs.writeFile(statisticsFile, JSON.stringify(statistics, null, 2));
+            }
+
+            const leagueCalendar = IcalGenerator.games(leagueName, games.filter(v => v.status === GameStatus.SCHEDULED || v.status === GameStatus.FINISHED), CONFIG.timezone, CONFIG.defaultGameDuration)
+
+            await fs.writeFile(path.resolve(leagueDirectory, 'games.ics'), leagueCalendar);
         }
-
-        const leagueCalendar = IcalGenerator.games(leagueName, games.filter(v => v.status === GameStatus.SCHEDULED || v.status === GameStatus.FINISHED), CONFIG.timezone, CONFIG.defaultGameDuration)
-
-        await fs.writeFile(path.resolve(leagueDirectory, 'games.ics'), leagueCalendar);
     }
 
     await fs.writeFile(path.resolve(baseOutputDir, 'weekly-games.json'), JSON.stringify(upcomingWeekGames, null, 2));
-})()
 
+    const aggregateStatistics = async (leagueSlug: string) => {
+        const seasonsDirectory = await fs.readdir(path.resolve(baseOutputDir, 'seasons'));
+        const statisticsByPlayerAndSeason: { [key: string]: { [key: string]: any } } = {};
+        const activeNames: string[] = [];
+
+        for (const season of seasonsDirectory) {
+            const previousSeasons: string[] = seasonsDirectory.map(v => v).sort().reverse().slice(0, CONFIG.aggregateYears);
+            console.log(previousSeasons)
+
+            try {
+                const statistics = JSON.parse(await fs.readFile(path.resolve(baseOutputDir, 'seasons', season, leagueSlug, 'statistics.json'), {encoding: "utf8"}));
+
+                for (const playerStatistics of statistics) {
+                    const name = playerStatistics.name;
+
+                    if (previousSeasons.includes(season) && !activeNames.includes(name)) {
+                        activeNames.push(name);
+                    }
+
+                    if (name) {
+                        if (!statisticsByPlayerAndSeason[name]) {
+                            statisticsByPlayerAndSeason[name] = {}
+                        }
+
+                        if (!statisticsByPlayerAndSeason[name][season]) {
+                            statisticsByPlayerAndSeason[name][season] = {}
+                        }
+
+                        statisticsByPlayerAndSeason[name][season] = playerStatistics.statistics
+                    }
+                }
+            } catch (err) {
+                console.warn(`No statistics.json found for ${season} ${leagueSlug}.`)
+                console.error(err)
+            }
+        }
+
+        for (const name of Object.keys(statisticsByPlayerAndSeason)) {
+            if (!activeNames.includes(name)) {
+                delete statisticsByPlayerAndSeason[name]
+            }
+        }
+
+        await fs.mkdir(path.resolve(baseOutputDir, 'statistics', leagueSlug), {recursive: true})
+        await fs.writeFile(path.resolve(baseOutputDir, 'statistics', leagueSlug, 'all.json'), JSON.stringify(statisticsByPlayerAndSeason, null, 2));
+    }
+
+    for (const league of CONFIG.leagues) {
+        await aggregateStatistics(league.slug);
+    }
+})()
 

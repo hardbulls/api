e901d58beb1d3c90fdbf579425d9ce8f3ba9bc85
Arjan Frans <arjanfrans.com@gmail.com>
2025-02-17 18:02:58 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -86,6 +86,20 @@ export const CONFIG: Config = {
             slug: "dqc-cup",
         },
         {
+            year: 2025,
+            name: 'Baseball Bundesliga',
+            shortName: 'BBL',
+            slug: 'bbl',
+            standings: 'https://www.baseballsoftball.at/de/events/baseball-bundesliga-2025/standings',
+            games: ['https://www.baseballsoftball.at/de/events/baseball-bundesliga-2025/calendars?round=&team=34514&date='],
+            statistics: {
+                batting: 'https://www.baseballsoftball.at/de/events/baseball-bundesliga-2025/stats/general/team/34514/all/batting',
+                pitching: 'https://www.baseballsoftball.at/de/events/baseball-bundesliga-2025/stats/general/team/34514/all/pitching',
+                fielding: 'https://www.baseballsoftball.at/de/events/baseball-bundesliga-2025/stats/general/team/34514/all/fielding'
+            },
+            logo: "bbl.svg"
+        },
+        {
             year: 2024,
             name: "Bulls Preseason",
             shortName: "Preseason",
